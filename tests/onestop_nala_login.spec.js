f6dec9297b4424c583121461b94e0b09c8349373
Steve Haight <steve.haight@aer.ca>
2017-05-29 12:57:03 -0600

    [SH] added onestop_nala_login spec

---
+++ tests/onestop_nala_login.spec.js
@@ -0,0 +1,79 @@
+/**
+ * Created by cb5rp on 5/26/2017.
+ */
+
+var expect = require('chai').expect,
+    test = require('selenium-webdriver/testing'),
+    webdriver = require('selenium-webdriver');
+
+//Load the chromedriver
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var By = webdriver.By;
+var until = webdriver.until;
+
+test.describe('Logon Page', function() {
+    var driver;
+
+    test.before(function(){
+        driver = new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .build();
+    });
+
+    test.after(function(){
+        driver.quit();
+    });
+
+    test.it('should display agreement disclaimer with correct credentials', function(done) {
+        driver.get('https://onestopuat.aer.ca/onestop/');
+
+        driver.findElement(By.name('username'))
+            .sendKeys('a50wadmin');
+        driver.findElement(By.name('password'))
+            .sendKeys('Nala2017');
+
+        driver.findElement(By.css('button.btn.btn-default.btn-login'))
+            .click();
+
+        driver.wait(function(){
+            return driver.findElement(By.css('div.modal')).then(function(element) {
+                return true;
+            },
+            function (error){
+                return false;
+            });
+        }, 5000);
+
+        driver.findElement(By.css('h4.modal-title')).then(function(element) {
+            expect(element).to.exist;
+            done();
+        });
+    });
+
+    test.it('should display login failure message with incorrect credentials', function(done) {
+        driver.get('https://onestopuat.aer.ca/onestop/');
+
+        driver.findElement(By.name('username'))
+            .sendKeys('xxx');
+        driver.findElement(By.name('password'))
+            .sendKeys('xxx');
+
+        driver.findElement(By.css('button.btn.btn-default.btn-login'))
+            .click();
+
+        driver.wait(function(){
+            return driver.findElement(By.css('div.login-error-message')).then(function(element) {
+                    expect(element).to.exist;
+                    done();
+                    return true;
+                },
+                function (error){
+                    return false;
+                });
+        }, 5000);
+    });
+});
